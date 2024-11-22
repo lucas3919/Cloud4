9737e7fc467d4db30d20067837a3894636fc7424
lucas3919 <lucasborgesdalcin@gmail.com>
2024-11-22 17:57:54 -0300

    intiial commit

--- terraform-eks/main.tf
+++ terraform-eks/main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
     region = "ca-central-1"
-    access_key = "AKIAUXTSPSNU5M5HZCLR"
-    secret_key = "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"
+
 }
 
 data "aws_eks_cluster" "eks-cluster" {
